3f5c89d02dcc5847557f2314425c5693e46225ce
Benoît Prieur <benoitprieur@Benoits-MacBook-Pro.local>
2024-12-10 15:48:49 -0700

    Deployment etc.

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -3,5 +3,11 @@ require("@nomicfoundation/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
